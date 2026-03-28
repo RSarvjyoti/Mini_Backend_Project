842e6cadd15b41ad3cb9e486e29a547fb92e33d2
RSarvjyoti <sarvjyoti64@gmail.com>
2026-03-28 09:49:29 +0530

    created posts APIs post and get

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,10 @@ const app = express();
 const multer = require("multer");
 const uploadFile = require("./services/storage.service");
 const Post = require("./models/post.model");
+const cors = require("cors");
 
 app.use(express.json());
+// app.use(cors());
 
 // upload file multer
 const upload = multer({
@@ -25,6 +27,14 @@ app.post('/create-post', upload.single("image"), async(req,res) => {
     return res.status(201).json({
         message: "created post",
         post: post
+    })
+})
+
+app.get("/posts", async(req, res) => {
+    const posts = await Post.find();
+    return res.status(200).json({
+        message: "Posts fetched!",
+        posts : posts
     })
 })
 
